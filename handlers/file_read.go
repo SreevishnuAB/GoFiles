c600f3699b3f3a5ff8b17a1e17ff24747eaf183b
Sreevishnu <sreeab28@gmail.com>
2021-04-12 06:27:01 +0530

    partial implementation of readFile

--- handlers/file_read.go
+++ handlers/file_read.go
@@ -3,12 +3,43 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/gorilla/mux"
+	"s.ab/gofiles/constants"
 )
 
 func GetFile(rw http.ResponseWriter, r *http.Request) {
-	fileName := mux.Vars(r)
+	fileName := mux.Vars(r)["fileName"] // mux.Vars() returns map of path params
 	log.Println("Queried file = ", fileName)
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+		// TODO proper error response
+		return
+	}
+
+	filePath := filepath.Join(wd, constants.FileDir, fileName)
+	log.Println("Queried filepath = ", filePath)
+	// file, err := os.Stat(filePath)
+	// if err != nil {
+	// 	if errors.Is(err, os.ErrNotExist){
+	// 		log.Println("Requested file not found")
+	// 		// TODO proper error response
+	// 	} else {
+	// 		log.Println(err)
+	// 	}
+	// 	return
+	// }
+
+	// log.Println("File details = ", file)
+	// TODO check if ReadFile can replace Stat
+	data, err := os.ReadFile(filePath)
+	log.Println(data)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
 }
